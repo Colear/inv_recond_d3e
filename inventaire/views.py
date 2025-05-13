afc66b89dcd0fc51d6babed974305515cf2372a1
colear <christophe.pinchon@lilo.org>
2025-05-13 13:19:38 +0200

    première vue

--- inventaire/views.py
+++ inventaire/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world ! Bienvenue à la racine de notre application d'inventaire !")
